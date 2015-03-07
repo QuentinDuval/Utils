2cd89c0a8910ae523298da7a2817f4f252eec9e2
QuentinDuval <senmenty@gmail.com>
2015-03-07 13:09:28 +0100

    Isolate the range

---
+++ include/utils/Range.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+namespace algorithm
+{
+   template<typename Iterator>
+   class Range
+   {
+   public:
+      Range(Iterator first, Iterator last)
+         : m_first(first), m_last(last) {}
+
+      Iterator begin()  const { return m_first; }
+      Iterator end()    const { return m_last; }
+
+      size_t   size()   const { return std::distance(m_first, m_last); }
+      bool     empty()  const { return m_first == m_last; }
+      void     pop()          { ++m_first; }
+
+   private:
+      Iterator m_first;
+      Iterator m_last;
+   };
+
+   template<typename Iterator>
+   bool operator== (Range<Iterator> const& lhs, Range<Iterator> const& rhs)
+   {
+      return begin(lhs) == begin(rhs) && end(lhs) == end(rhs);
+   }
+
+   template<typename Iterator>
+   Range<Iterator> makeRange(Iterator first, Iterator last)
+   {
+      return Range<Iterator>(first, last);
+   }
+}
--- include/utils/Utils.h
+++ include/utils/Utils.h
@@ -8,35 +8,6 @@
 namespace algorithm
 {
    template<typename Iterator>
-   class Range
-   {
-   public:
-      Range(Iterator first, Iterator last)
-         : m_first(first), m_last(last) {}
-
-      Iterator begin()  const { return m_first; }
-      Iterator end()    const { return m_last; }
-
-      size_t   size()   const { return std::distance(m_first, m_last); }
-      bool     empty()  const { return m_first == m_last; }
-      void     pop()          { ++m_first; }
-
-   private:
-      Iterator m_first;
-      Iterator m_last;
-   };
-
-   template<typename Iterator>
-   Range<std::reverse_iterator<Iterator>> reverseRange(Range<Iterator> const& r)
-   {
-      std::reverse_iterator<Iterator> rbegin(r.end());
-      std::reverse_iterator<Iterator> rend(r.begin());
-      return Range<std::reverse_iterator<Iterator>>(rbegin, rend);
-   }
-
-   //--------------------------------------------------------------------------
-
-   template<typename Iterator>
    void advance(Iterator& current, Iterator end,
       typename std::iterator_traits<Iterator>::difference_type n = 1)
    {
