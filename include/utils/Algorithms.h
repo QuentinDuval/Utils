804547934fa259cc23e4c04bc59c204befd26617
QuentinDuval <senmenty@gmail.com>
2015-03-07 13:30:18 +0100

    More algorithms

--- include/utils/Algorithms.h
+++ include/utils/Algorithms.h
@@ -22,11 +22,97 @@ namespace utils
       generate(begin(cont), end(cont), val, next);
    }
 
+   //--------------------------------------------------------------------------
+
+   template<typename Container, typename Output, typename Tranformation>
+   Output transform(Container& c, Output out, Tranformation tf)
+   {
+      return std::transform(begin(c), end(c), out, tf);
+   }
+
+   template<typename Container, typename Container2, typename Output, typename Tranformation>
+   Output zipWith(Container& c, Container2& c2, Output out, Tranformation tf)
+   {
+      return std::transform(begin(c), end(c), begin(c2), out, tf);
+   }
+
+   template<typename Container, typename Output>
+   Output copyTo(Container& c, Output out)
+   {
+      return std::copy(begin(c), end(c), out);
+   }
+
+   //--------------------------------------------------------------------------
+
+   template<typename Container, typename Less>
+   void sortBy(Container& cont, Less less)
+   {
+      std::sort(begin(cont), end(cont), less);
+   }
+
+   //--------------------------------------------------------------------------
+
+   template<typename Container, typename Less>
+   typename Container::const_iterator minBy(Container const& cont, Less less)
+   {
+      return std::min_element(begin(cont), end(cont), less);
+   }
+
+   template<typename Container, typename Less>
+   typename Container::const_iterator maxBy(Container const& cont, Less less)
+   {
+      return std::max_element(begin(cont), end(cont), less);
+   }
+
+   template<typename Container, typename Less>
+   typename Container::iterator minBy(Container& cont, Less less)
+   {
+      return std::min_element(begin(cont), end(cont), less);
+   }
+
+   template<typename Container, typename Less>
+   typename Container::iterator maxBy(Container& cont, Less less)
+   {
+      return std::max_element(begin(cont), end(cont), less);
+   }
+
+   //--------------------------------------------------------------------------
+
+   template<typename Container, typename Val>
+   bool contains(Container const& cont, Val val)
+   {
+      return end(cont) != std::find(begin(cont), end(cont), val);
+   }
+
+   template<typename Container, typename Predicate>
+   bool containsIf(Container const& cont, Predicate pred)
+   {
+      return end(cont) != std::find_if(begin(cont), end(cont), pred);
+   }
+
    template<typename Container, typename Predicate>
    typename Container::const_iterator findIf(Container const& cont, Predicate pred)
    {
       return std::find_if(begin(cont), end(cont), pred);
    }
+
+   //--------------------------------------------------------------------------
+
+   template<typename Container, typename Val>
+   void erase(Container& cont, Val const& v)
+   {
+      auto it = std::remove(begin(cont), end(cont), v);
+      cont.erase(it, end(cont));
+   }
+
+   template<typename Container, typename Predicate>
+   void eraseIf(Container& cont, Predicate p)
+   {
+      auto it = std::remove_if(begin(cont), end(cont), p);
+      cont.erase(it, end(cont));
+   }
+
+   //--------------------------------------------------------------------------
 
    template<typename Container, typename Container2>
    bool equal(Container& cont, Container2& cont2)
@@ -34,17 +120,45 @@ namespace utils
       return std::equal(begin(cont), end(cont), begin(cont2));
    }
 
-   template<typename Container, typename Value, typename Accumulator>
-   Value accumulate(Container const& cont, Value const& val, Accumulator acc)
+   //--------------------------------------------------------------------------
+
+   template<typename Container, typename Val, typename Acc>
+   Val accumulate(Container& c, Val const& v, Acc acc)
    {
-      return std::accumulate(begin(cont), end(cont), val, acc);
+      return std::accumulate(begin(c), end(c), v, acc);
    }
 
-   template<typename Container, typename Lesser>
-   typename Container::const_iterator minBy(Container const& cont, Lesser less)
+   template<typename Container, typename Output, typename Val>
+   void intersperse(Container& cont, Output out, Val const& v)
    {
-      return std::min_element(begin(cont), end(cont), less);
+      bool first = true;
+      for (auto& c : cont)
+      {
+         if (!first)
+         {
+            *out = v;
+            ++out;
+         }
+
+         first = false;
+         *out = c;
+         ++out;
+      }
    }
+
+   template<typename Container, typename Val>
+   Val sum(Container& c, Val const& v)
+   {
+      return accumulate(c, v, std::plus<Val>());
+   }
+
+   template<typename Container, typename Val>
+   Val product(Container& c, Val const& v)
+   {
+      return accumulate(c, v, std::multiplies<Val>());
+   }
+
+   //--------------------------------------------------------------------------
 
    template<typename Iterator>
    void shuffle(Iterator first, Iterator last)
